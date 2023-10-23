d5b05a840f3157d8638e0ff139226f831b803652
juan1david <juancr200502@gmail.com>
2023-10-23 12:58:39 -0500

    agrega proyecto

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ const colors = require('colors');
 
 const {mostrarMenu, pausa} = require('./node_modules/menu.js');
 
+console.log('Este programa muestra un menú principal con varias opciones,' +
+'permitiendo al usuario realizar acciones específicas en función de su elección.');
+
+
 const main = async() => {
     console.clear();
     console.log('******************************');
